da00e0e13cb60e162df9858080ca946df6ec6f1e
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-10-31 00:02:25 +0200

    Update project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     >
       <div id="nav" data-section="nav">
         <NavbarStyleApple
-          brandName="Are u done ?"
+          brandName="Lesgoooo"
           navItems={[
             { name: "Menu", id: "product" },
             { name: "About", id: "about" },
