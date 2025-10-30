c0714abf8f40bd0f04ecbca572a808ddb09d67f4
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-10-30 23:41:37 +0200

    Update project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
 
       <div id="hero" data-section="hero">
         <HeroSplit
-          title="Maybe not"
+          title="IDK"
           description="Lets go"
           tag="Est. 1985"
           tagIcon={Pizza}
